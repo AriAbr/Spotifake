87e2a6632d474236d75ff20d7afb80b294253e82
Ari <ari.abramowitz1@gmail.com>
2018-12-25 16:52:22 -0500

    assignment 2755 routing: completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
