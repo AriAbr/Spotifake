a1db885d22da4562eee989de2ad9d47538b0cdd6
Ari <ari.abramowitz1@gmail.com>
2018-12-27 17:13:27 -0500

    checkpoint 2759 audio playback: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -8,10 +8,48 @@ class Album extends Component {
       return album.slug === this.props.match.params.slug
     });
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying:true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
+
   render() {
+    const songs = this.state.album.songs.map( (song, index) =>
+      <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+        <td>{index+1}</td>
+        <td>{song.title}</td>
+        <td>{song.duration}</td>
+      </tr>
+    )
     return (
       <section className="album">
         <section id="album-info">
@@ -28,13 +66,7 @@ class Album extends Component {
             <col id="song-duration-column" />
             </colgroup>
             <tbody>
-              {this.state.album.songs.map( (song, index) =>
-                <tr key={index}>
-                  <td>{index+1}</td>
-                  <td>{song.title}</td>
-                  <td>{song.duration}</td>
-                </tr>
-              )}
+              { songs }
             </tbody>
           </table>
         </section>
